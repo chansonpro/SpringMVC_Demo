e602662ab9569213e08abba8362fee4bb8971887
Chanson <chansonpro@outlook.com>
2017-08-11 15:13:03 +0800

    test

--- src/com/test/model/Test.java
+++ src/com/test/model/Test.java
@@ -1,10 +1,15 @@
 package com.test.model;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by: zhangsong09@baidu.com
  * Date-Time: 2017/8/11 14:48
  */
 public class Test {
+    public void test(){
+        System.out.println("nihao");
+    }
 
 }
 
